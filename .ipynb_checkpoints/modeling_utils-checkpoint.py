15e40d8681fbb47f1503f7c069d26ebdd67cac20
Ivan <ivan.zakharchuk@gmail.com>
2021-02-16 19:03:15 -0500

    Created Linear Model in modeling

--- .ipynb_checkpoints/modeling_utils-checkpoint.py
+++ .ipynb_checkpoints/modeling_utils-checkpoint.py
@@ -7,6 +7,30 @@ from geopy.distance import distance
 import re
 
 
+def categor_var_analysis(df, features):
+    """
+    Features - list of categorical features
+    df - data frame where features located
+    """
+    print('Discrete Variables')
+    
+    for feature in features:
+        print(df.groupby('{}'.format(feature))['{}'.format(feature)].count())
+        print('--'*40)
+        
+
+def numeric_var_analysis(df, features):
+    """
+    Features - list of numerical features
+    df - data frame where features located
+    """
+    print('Continuous Variables')
+    print(df[features].describe().transpose())
+    
+
+
+    
+    
 
 
 def to_dummies(df, features):
@@ -17,7 +41,7 @@ def to_dummies(df, features):
     """
     d_df = []
     for f in features:
-        d_df.append(pd.get_dummies(df[f], prefix='{}'.format(str(f)[:5]), drop_first=True))
+        d_df.append(pd.get_dummies(df[f], prefix='{}'.format(str(f)[:]), drop_first=True))
     #import pdb;pdb.set_trace()
     df = df.drop(features, axis = 1)
     df = pd.concat([df] + d_df ,axis=1)
