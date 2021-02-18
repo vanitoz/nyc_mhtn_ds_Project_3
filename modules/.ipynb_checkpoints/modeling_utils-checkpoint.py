b52fcf2504c3fcf1c3cc2397bf013c716e21bef5
Ivan <ivan.zakharchuk@gmail.com>
2021-02-18 14:44:49 -0500

    Visualizations

--- modules/.ipynb_checkpoints/modeling_utils-checkpoint.py
+++ modules/.ipynb_checkpoints/modeling_utils-checkpoint.py
@@ -15,10 +15,12 @@ from imblearn.over_sampling import SMOTE, ADASYN
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import BaggingClassifier, RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier, GradientBoostingClassifier
 
 from sklearn import metrics
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import plot_confusion_matrix, recall_score, precision_score, classification_report
+
 
 from ingeniring_utils import * # import all existing functions from file cleaning.py
 from modeling_utils import *
@@ -273,12 +275,12 @@ def AdaBoost(X, y):
     
     return adaboost_clf
 
-def gbt(X, y):
+def gbt(X, y, max_depth = None, show_roc = False):
     
     # Split the data into training and test sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
     
-    gbt_clf = GradientBoostingClassifier(random_state=42)
+    gbt_clf = GradientBoostingClassifier(random_state=42, max_depth = max_depth)
     gbt_clf.fit(X_train, y_train).fit(X_train, y_train)
     
     # AdaBoost model predictions
@@ -299,81 +301,26 @@ def gbt(X, y):
     print("\n General Repor :\n")
     print(classification_report(y_test, gbt_clf_test_preds))
     
-    return adaboost_clf
-    
-    
-    
-    
-# def decision_tree(X, y, max_depth, future_importance = True, confusion_mtrix = True, report =True):
-    
-#     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 0, test_size = 0.25)
-    
-#     tree_clf = DecisionTreeClassifier(max_depth = 3)
-#     tree_clf.fit(X_train,y_train)
-    
-    
-#     tree_clf.feature_importances_
-#     if future_importance:
-#         print('Feature Importance: \n')
-#         n_features = X_train.shape[1]
-#         plt.figure(figsize=(8,8))
-#         plt.barh(range(n_features), tree_clf.feature_importances_, align='center') 
-#         plt.yticks(np.arange(n_features), X_train.columns.values) 
-#         plt.xlabel('Feature importance')
-#         plt.ylabel('Feature')
-#         plt.show()
+    if show_roc:
         
-#     # Test set predictions
-#     pred = tree_clf.predict(X_test)
-    
-#     if confusion_mtrix:
-#         print('Confusion Matrix: \n')
-#         # Confusion matrix and classification report
-#         print(confusion_matrix(y_test,pred))
-        
-#     if report:
-#         print('\nGeneral Report \n')
-#         print(classification_report(y_test,pred))
+        y_score = gbt_clf.decision_function(X_test)
+        fpr, tpr, thresholds = roc_curve(y_test, y_score)
+        print('AUC: {}'.format(auc(fpr, tpr)))
+        plt.figure(figsize=(10, 8))
+        lw = 2
+        plt.plot(fpr, tpr, color='darkorange', lw=lw, label='ROC curve')
+        plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.yticks([i/20.0 for i in range(21)])
+        plt.xticks([i/20.0 for i in range(21)])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('Receiver operating characteristic (ROC) Curve')
+        plt.legend(loc='lower right')
+        plt.show()
 
-#     return print("Testing Accuracy for Decision Tree Classifier: {:.4}%".format(accuracy_score(y_test, pred) * 100))
-
-
-
-
-# def decision_tree_smote(X, y, max_depth, future_importance = True, confusion_mtrix = True, report =True):
     
-#     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 0, test_size = 0.25)
-    
-#     X_train_resampled, y_train_resampled = SMOTE().fit_resample(X_train, y_train) 
-
-#     tree_clf = DecisionTreeClassifier(max_depth = 3)
-#     tree_clf.fit(X_train_resampled,y_train_resampled)
+    return gbt_clf
     
     
-#     tree_clf.feature_importances_
-#     if future_importance:
-#         print('Feature Importance: \n')
-#         n_features = X_train.shape[1]
-#         plt.figure(figsize=(8,8))
-#         plt.barh(range(n_features), tree_clf.feature_importances_, align='center') 
-#         plt.yticks(np.arange(n_features), X_train.columns.values) 
-#         plt.xlabel('Feature importance')
-#         plt.ylabel('Feature')
-#         plt.show()
-        
-        
-#     # Test set predictions
-#     pred = tree_clf.predict(X_test)
-    
-#     if confusion_mtrix:
-#         print('Confusion Matrix: \n')
-#         # Confusion matrix and classification report
-#         print(confusion_matrix(y_test,pred))
-        
-#     if report:
-#         print('\nGeneral Report \n')
-#         print(classification_report(y_test,pred))
-    
-#     print("Testing Accuracy for Decision Tree Classifier: {:.4}%".format(accuracy_score(y_test, pred) * 100))
-    
-#     return tree_clf
